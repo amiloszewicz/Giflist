6d307def6795ebb816b6755283575410015420a4
amiloszewicz <miloszewicz.adam@gmail.com>
2024-07-04 12:45:30 +0200

    feat: home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `<p>home works!</p>`,
+  styles: ``,
+})
+export default class HomeComponent {}
