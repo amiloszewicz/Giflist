b8942c40724febaca91cd3f50a8afb462df046c0
amiloszewicz <miloszewicz.adam@gmail.com>
2024-07-12 14:38:08 +0200

    feat: progress-spinner for loading gif-list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { RedditService } from '../shared/data-access/reddit.service';
 import { GifListComponent } from './ui/gif-list.component';
@@ -11,6 +12,12 @@ import { SearchBarComponent } from './ui/search-bar.component';
     <app-search-bar
       [subredditFormControl]="redditService.subredditFormControl"
     ></app-search-bar>
+    @if (redditService.loading()) {
+    <mat-progress-spinner
+      mode="indeterminate"
+      diameter="50"
+    ></mat-progress-spinner>
+    } @else {
     <app-gif-list
       [gifs]="redditService.gifs()"
       infiniteScroll
@@ -18,8 +25,13 @@ import { SearchBarComponent } from './ui/search-bar.component';
         onScroll(); redditService.pagination$.next(redditService.lastKnowGif())
       "
       class="grid-container"
-    />`,
-  imports: [GifListComponent, InfiniteScrollModule, SearchBarComponent],
+    />}`,
+  imports: [
+    GifListComponent,
+    InfiniteScrollModule,
+    SearchBarComponent,
+    MatProgressSpinnerModule,
+  ],
   styles: ``,
 })
 export default class HomeComponent {
