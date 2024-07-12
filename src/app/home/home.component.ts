bd2ee35819216b0d8be878d056b9719af086586a
amiloszewicz <miloszewicz.adam@gmail.com>
2024-07-12 12:21:16 +0200

    feat: infiniteScroll to gif-list with load pagination from last gif

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { RedditService } from '../shared/data-access/reddit.service';
 import { GifListComponent } from './ui/gif-list.component';
 
@@ -6,10 +7,20 @@ import { GifListComponent } from './ui/gif-list.component';
   selector: 'app-home',
   standalone: true,
   template: ` <h1>Giflist</h1>
-    <app-gif-list [gifs]="redditService.gifs()" class="grid-container" />`,
-  imports: [GifListComponent],
+    <app-gif-list
+      [gifs]="redditService.gifs()"
+      infiniteScroll
+      (scrolled)="
+        onScroll(); redditService.pagination$.next(redditService.lastKnowGif())
+      "
+      class="grid-container"
+    />`,
+  imports: [GifListComponent, InfiniteScrollModule],
   styles: ``,
 })
 export default class HomeComponent {
   redditService = inject(RedditService);
+  onScroll() {
+    console.log('scrolled!!', this.redditService.lastKnowGif());
+  }
 }
